de693c307e6db83754312b10d24a01d09cf8de7e
pm58 <apple.zy@me.com>
2018-12-08 22:18:39 +0800

    hikyuu

---
+++ hikyuu/examples/web/hikyuu.readthedocs.io/en/latest/_static/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "hikyuu",
+    version: "latest",
+    language: "en",
+    subprojects: {},
+    canonical_url: "http://hikyuu.readthedocs.io/en/latest/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/source/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "a0b7388d"
+  };
+  
